a6bade2836ddd6a495b237c3eeed4cebbe914091
manideeppabba1991 <manideeppabba@gmail.com>
2017-02-13 11:03:10 -0600

    creating new angular 2 app

---
+++ Angular2_by_manideep/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'pc-app',
+    templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent{
+    pageTitle: string = "Practice Club";
+}
